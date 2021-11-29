d87f15c7d3809b075129d7511e74583e367b8f94
Vlad-Alexandru Zumer <zumer.vlad@yahoo.ro>
2021-11-29 01:48:45 +0000

    day12

---
+++ y-2020/day-12.hs
@@ -0,0 +1,116 @@
+inputData1 :: [String]
+inputData1 = [ "W2","N4","R90","E3","N2","W4","S5","F83","E5","F53","S3","L90","E1","S2","N2","W5","E4","L180","E4","N1","F27","L90","F9","E3","N2","N3","R90","N5","F57","W5","R180","R180","W5","F44","L90","E5","F87","R180","F61","E4","F37","E2","F39","L180","F53","S1","W1","S2","E2","L90","W4","N5","E1","S1","F31","L90","W5","L180","W1","N5","R90","N5","R90","F94","S5","R90","S2","F94","S3","E1","E5","F9","L90","W5","F83","N2","N5","L90","F33","W4","L90","E5","S5","F23","W5","N1","E3","S1","N1","F59","N1","E1","S2","F56","S2","E5","R180","S4","R180","F46","L90","F78","E5","L180","S4","F22","S5","F32","L90","F68","L90","S3","F76","E3","F71","R90","F34","L90","W5","R90","F12","F65","N4","W5","F65","R270","F13","W2","S2","R90","N1","F14","L180","W4","N5","R180","N2","R90","S3","F1","W2","F8","L90","F98","N5","E3","R90","N3","F39","L180","F87","E3","R180","E4","R90","W4","L180","W2","L90","S1","W2","R180","N3","L90","W4","S4","L90","S4","F75","R90","R180","N4","E5","F9","F40","S3","R90","S2","F26","E2","L180","S4","N5","W1","S5","W3","F11","E2","N5","W3","S5","R90","N2","E4","L90","R90","F8","E4","R90","N2","L90","N3","F8","E2","F67","W5","F19","S3","L90","S3","L90","W1","F54","S1","R90","S4","E1","S3","L90","F14","W4","W3","F36","E5","R90","F10","W2","S1","W2","N5","W4","F64","W5","S4","F13","E5","N1","F87","E3","S4","E5","W3","F46","S5","R270","S4","E3","R90","F97","F92","E2","F17","R90","F5","N1","F89","N5","F55","R90","F51","S3","F97","L90","W5","R90","F7","L180","L180","W5","F88","W2","F26","R180","S4","F54","S1","R90","F66","R90","F6","L90","N5","L90","R90","F58","E3","F67","S1","R90","W4","N4","L90","F63","E3","R90","E4","N4","L180","N3","F34","E5","R90","W1","R90","N3","F73","N5","R90","F28","W1","W3","F38","N3","E1","S5","S2","F72","R90","F25","N3","E2","S3","F63","L270","N3","E5","R90","N4","E3","S1","F32","S5","W3","F98","E2","S5","L90","N5","W4","L90","F68","E2","F81","N2","E4","L90","E1","L90","E1","L180","W3","F99","R90","W1","S4","L90","S4","R90","N2","F17","E3","F78","W1","S2","L180","N5","L90","N2","E4","L90","W1","N2","F97","W3","S5","L180","S4","F77","L90","F55","W3","N4","E4","R90","E5","S3","L90","E1","R90","F54","L90","N5","E4","R90","F41","L90","N1","R90","E5","R180","W2","F74","L90","F88","N3","F25","L180","E2","S1","W4","N1","W5","R180","F31","E1","R180","F17","N1","W2","R180","F61","L270","W4","L180","F66","E4","F68","L90","W4","L180","E4","S1","F30","S3","E1","F93","L90","F33","N3","L90","F58","R90","R90","F23","N5","W2","N3","W4","L180","N1","F84","W5","E5","F36","W3","N3","W3","R180","W2","S3","E4","F62","L90","S2","W4","F28","E1","S5","F54","S5","R270","F35","N4","R90","F38","W4","S3","W2","R90","N2","L270","F21","R90","W5","R180","F7","W1","F72","E3","L180","E1","F42","L270","F1","R90","E4","F72","W3","R90","E4","S4","W4","R90","F98","R90","F100","R90","E1","F9","N1","F81","S5","L90","L90","W3","L90","F75","L90","F27","E3","L90","F49","F53","L90","F26","W1","F48","W1","L90","W1","L90","F71","S1","F34","S1","L90","S2","N3","L180","E1","F52","S5","R90","E4","F58","W2","R90","E5","N3","R180","F56","L90","F92","S1","E2","F68","F24","N3","F29","S4","L90","N5","L90","F48","S5","F80","R90","F34","S5","F23","F36","W2","F57","W5","N1","S2","R90","F94","L90","N2","F95","R180","N1","W1","F59","N5","F62","S4","L90","N4","E2","F55","L90","F21","E2","F52","W2","R90","N3","W5","S1","L90","W1","R90","R90","F21","E4","F47","E5","N5","W3","F34","F2","N1","L90","S3","R90","W1","N4","F49","W1","F15","E5","R90","S4","F39","N4","R90","N4","F69","E2","N5","R90","F21","W5","S5","E4","S3","F67","E3","S2","R90","F51","L90","N5","F73","S1","F18","R180","W2","N1","W5","L90","W2","R90","E2","L90","W3","L90","F13","L90","F45","R90","F85","E2","F44","F65","L90","F82","W2","L270","F65","N3","W3","R90","E3","F20","R90","S2","S3","R180","N4","F98","W5","S2","F63","R90","F88","W3","F1","S4","F39","R180","N3","F84","N4","F51","E1","N5","E3","F70","L90","N3","L180","F63","S2","L90","F16","F11","R180","F70","E2","L90","F46","N2","E1","S1","F19","N5","W1","F67","R90","F79","S2","W5","F96","N1","F53","E3","R90","E1","F78","L90","F61","E5","F85","L90","W4","F72","W1","S5","F49","W1","N1","E2","R90","E2","L90","S5","R90","E2","S4","E3","F8","R90","N3","L90","W1","F56","E1","W4","N5","R90","F47","R90","W1","R90","W5","F5"]
+
+data Heading = N | E | S | W deriving (Show, Eq)
+type Position = (Int, Int) -- (x,y),  E = right (x+1,y), N = up (x,y+1)
+data Ship = Ship Position Heading deriving (Show)
+
+data Action = HeadingAction Heading | F | R | L deriving (Show)
+type Command = (Action, Int)
+
+initialShipPos :: Position
+initialShipPos = (0,0)
+
+initialShip :: Ship
+initialShip = Ship initialShipPos E
+
+strToCommand :: String -> Command
+strToCommand "" = error "Cannot make command of empty"
+strToCommand [com] = error "Command is missing arguments"
+strToCommand ('N':(num:xs)) = (HeadingAction N, read(num:xs)::Int)
+strToCommand ('E':(num:xs)) = (HeadingAction E, read(num:xs)::Int)
+strToCommand ('S':(num:xs)) = (HeadingAction S, read(num:xs)::Int)
+strToCommand ('W':(num:xs)) = (HeadingAction W, read(num:xs)::Int)
+strToCommand ('F':(num:xs)) = (F, read(num:xs)::Int)
+strToCommand ('R':(num:xs)) = (R, read(num:xs)::Int)
+strToCommand ('L':(num:xs)) = (L, read(num:xs)::Int)
+strToCommand (com:(num:xs)) = error ("Unrecognized command " ++ [com]);
+
+executeCommand1 :: Ship -> Command -> Ship
+executeCommand1 (Ship pos heading) (HeadingAction N, n) = Ship (addPos pos (getAbsVec N n)) heading
+executeCommand1 (Ship pos heading) (HeadingAction E, n) = Ship (addPos pos (getAbsVec E n)) heading
+executeCommand1 (Ship pos heading) (HeadingAction S, n) = Ship (addPos pos (getAbsVec S n)) heading
+executeCommand1 (Ship pos heading) (HeadingAction W, n) = Ship (addPos pos (getAbsVec W n)) heading
+executeCommand1 (Ship pos heading) (F, n) = Ship (addPos pos (getAbsVec heading n)) heading
+executeCommand1 (Ship pos heading) (R, n)
+    | n `mod` 90 == 0 = Ship pos (rotateRTimes heading (n `div` 90))
+    | otherwise = error "Cannot ue angle that is not divisible by 90"
+executeCommand1 (Ship pos heading) (L, n)
+    | n `mod` 90 == 0 = Ship pos (rotateLTimes heading (n `div` 90))
+    | otherwise = error "Cannot ue angle that is not divisible by 90"
+
+addPos :: Position -> Position -> Position
+addPos (x1,y1) (x2,y2) = (x1+x2, y1+y2)
+
+getAbsVec :: Heading -> Int -> Position
+getAbsVec N n = (0,n)
+getAbsVec E n = (n,0)
+getAbsVec S n = (0,-n)
+getAbsVec W n = (-n,0)
+
+rotateLTimes :: Heading -> Int -> Heading
+rotateLTimes heading 0 = heading
+rotateLTimes N n
+    | n > 0 = rotateLTimes W (n-1)
+    | n < 0 = rotateRTimes N (-n)
+rotateLTimes E n
+    | n > 0 = rotateLTimes N (n-1)
+    | n < 0 = rotateRTimes E (-n)
+rotateLTimes S n
+    | n > 0 = rotateLTimes E (n-1)
+    | n < 0 = rotateRTimes S (-n)
+rotateLTimes W n
+    | n > 0 = rotateLTimes S (n-1)
+    | n < 0 = rotateRTimes W (-n)
+
+rotateRTimes :: Heading -> Int -> Heading
+rotateRTimes heading 0 = heading
+rotateRTimes N n
+    | n > 0 = rotateRTimes E (n-1)
+    | n < 0 = rotateLTimes N (-n)
+rotateRTimes E n
+    | n > 0 = rotateRTimes S (n-1)
+    | n < 0 = rotateLTimes E (-n)
+rotateRTimes S n
+    | n > 0 = rotateRTimes W (n-1)
+    | n < 0 = rotateLTimes S (-n)
+rotateRTimes W n
+    | n > 0 = rotateRTimes N (n-1)
+    | n < 0 = rotateLTimes W (-n)
+
+type Waypoint = Ship
+
+initialWaypoint :: Position
+initialWaypoint = (10,1)
+
+executeCommand2 :: (Position, Position) -> Command -> (Position, Position)
+executeCommand2 (shippos, waypointpos) (HeadingAction N, n) = (shippos, (addPos waypointpos (getAbsVec N n)))
+executeCommand2 (shippos, waypointpos) (HeadingAction E, n) = (shippos, (addPos waypointpos (getAbsVec E n)))
+executeCommand2 (shippos, waypointpos) (HeadingAction S, n) = (shippos, (addPos waypointpos (getAbsVec S n)))
+executeCommand2 (shippos, waypointpos) (HeadingAction W, n) = (shippos, (addPos waypointpos (getAbsVec W n)))
+executeCommand2 ((shipx, shipy),(waypointx, waypointy)) (F, n) = ((shipx + waypointx * n, shipy + waypointy * n),(waypointx, waypointy))
+executeCommand2 (shippos, waypointpos) (R, n)
+    | n `mod` 90 == 0 = (shippos, rotateWaypointR waypointpos (n `div` 90))
+    | otherwise = error "Cannot ue angle that is not divisible by 90"
+executeCommand2 (shippos, waypointpos) (L, n)
+    | n `mod` 90 == 0 = (shippos, rotateWaypointL waypointpos (n `div` 90))
+    | otherwise = error "Cannot ue angle that is not divisible by 90"
+
+rotateWaypointR :: Position -> Int -> Position
+rotateWaypointR pos 0 = pos
+rotateWaypointR (x,y) n
+    | n > 0 =  rotateWaypointR (y,-x) (n-1)
+    | n < 0 = rotateWaypointL (x,y) (-n)
+
+-- (x+iy) * i => -y+xi
+rotateWaypointL :: Position -> Int -> Position
+rotateWaypointL pos 0 = pos
+rotateWaypointL (x,y) n
+    | n > 0 =  rotateWaypointL (-y,x) (n-1)
+    | n < 0 = rotateWaypointR (x,y) (-n)
+
+main :: IO()
+main = do
+    print (foldl executeCommand1 initialShip (map strToCommand inputData1))
+    print (foldl executeCommand2 (initialShipPos, initialWaypoint) (map strToCommand inputData1))
+    print "Done"
