da08398676516f9a7fb3fde01742259806f62145
Vlad Zumer <21068598+Vlad-Zumer@users.noreply.github.com>
2023-09-08 18:38:19 +0100

    update performance utilities

--- utils/cpp-utils-perf.hpp
+++ utils/cpp-utils-perf.hpp
@@ -1,9 +1,41 @@
 #ifndef CPP_VZ_UTILS_PERF
 #define CPP_VZ_UTILS_PERF
 
-#include<string>
 #include<functional>
 #include<chrono>
+#include<utility>
+
+//////////////////////////////////////////////
+//          Wrapper Examples
+//////////////////////////////////////////////
+//      template<class TOut, class... FuncTsIn, class... WrapperTsIn>
+//      TOut wrap(TOut(*func)(FuncTsIn...), WrapperTsIn... args)
+//      {
+//          std::cout<<"Wrapped function MFER!\n";
+//          return func(std::forward<WrapperTsIn>(args)...);
+//      }
+//
+//      template<class CL, class TOut, class... FuncTsIn, class... WrapperTsIn>
+//      TOut wrap(CL* self, TOut(CL::*func)(FuncTsIn...), WrapperTsIn... args)
+//      {
+//          std::cout<<"Wrapped method MFER!\n";
+//          return (self->*func)(std::forward<WrapperTsIn>(args)...);
+//      }
+//
+//      template<class CL, class TOut, class... FuncTsIn, class... WrapperTsIn>
+//      TOut wrap(const CL* self, TOut(CL::*func)(FuncTsIn...) const, WrapperTsIn... args)
+//      {
+//          std::cout<<"Wrapped const method MFER!\n";
+//          return (self->*func)(std::forward<WrapperTsIn>(args)...);
+//      }
+//
+//      template<class TOut, class... FuncTsIn, class... WrapperTsIn>
+//      TOut wrap(std::function<TOut(FuncTsIn...)> func, WrapperTsIn... args)
+//      {
+//          std::cout<<"Wrapped with std::function MFER!\n";
+//          return func(std::forward<WrapperTsIn>(args)...);
+//      }
+//////////////////////////////////////////////
 
 namespace Utils
 {
@@ -16,31 +48,120 @@ namespace Utils
         T Value;
     };
 
-    template<typename T>
-    TimedReturnedObject<T> TimeFunction(T(*func)())
-    {        
+    template<class TOut, class... FuncTsIn, class... WrapperTsIn>
+    TimedReturnedObject<TOut> TimeFunction(TOut(*func)(FuncTsIn...), WrapperTsIn... args)
+    {
+        TimedReturnedObject<TOut> retObj;
+
         auto start = chrono::high_resolution_clock::now();
-        T val = func();
+        retObj.Value = func(std::forward<WrapperTsIn>(args)...);
         auto end = chrono::high_resolution_clock::now();
 
-        TimedReturnedObject<T> retObj;
         retObj.Nanoseconds = end - start;
-        retObj.Value = val;
-
         return retObj;
     }
 
-    template<typename T>
-    TimedReturnedObject<T> TimeFunction_f(function<T()> func)
+    template<class TOut, class... FuncTsIn, class... WrapperTsIn>
+    TimedReturnedObject<TOut> TimeFunction(std::function<TOut(FuncTsIn...)> func, WrapperTsIn... args)
     {
+        TimedReturnedObject<TOut> retObj;
+
         auto start = chrono::high_resolution_clock::now();
-        T val = func();
+        retObj.Value = func(std::forward<WrapperTsIn>(args)...);
         auto end = chrono::high_resolution_clock::now();
 
-        TimedReturnedObject<T> retObj;
         retObj.Nanoseconds = end - start;
-        retObj.Value = val;
+        return retObj;
+    }
 
+    template<class CL, class TOut, class... FuncTsIn, class... WrapperTsIn>
+    TimedReturnedObject<TOut> TimeFunction(CL* self, TOut(CL::*func)(FuncTsIn...), WrapperTsIn... args)
+    {
+        TimedReturnedObject<TOut> retObj;
+
+        auto start = chrono::high_resolution_clock::now();
+        retObj.Value = (self->*func)(std::forward<WrapperTsIn>(args)...);
+        auto end = chrono::high_resolution_clock::now();
+
+        retObj.Nanoseconds = end - start;
+        return retObj;
+    }
+    
+    template<class CL, class TOut, class... FuncTsIn, class... WrapperTsIn>
+    TimedReturnedObject<TOut> TimeFunction(const CL* self, TOut(CL::*func)(FuncTsIn...) const, WrapperTsIn... args)
+    {
+        TimedReturnedObject<TOut> retObj;
+
+        auto start = chrono::high_resolution_clock::now();
+        retObj.Value = (self->*func)(std::forward<WrapperTsIn>(args)...);
+        auto end = chrono::high_resolution_clock::now();
+
+        retObj.Nanoseconds = end - start;
+        return retObj;
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
+    //         DEAL WITH "void" NOT BEING A PROPER RETURN TYPE
+    //              TEMPLATE SPECIALIZATION FOR "void"
+    ////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
+
+    template<>
+    struct TimedReturnedObject<void>
+    {
+        chrono::nanoseconds Nanoseconds;
+    };
+
+    template<class... FuncTsIn, class... WrapperTsIn>
+    TimedReturnedObject<void> TimeFunction(void(*func)(FuncTsIn...), WrapperTsIn... args)
+    {
+        TimedReturnedObject<void> retObj;
+
+        auto start = chrono::high_resolution_clock::now();
+        func(std::forward<WrapperTsIn>(args)...);
+        auto end = chrono::high_resolution_clock::now();
+
+        retObj.Nanoseconds = end - start;
+        return retObj;
+    }
+
+    template<class... FuncTsIn, class... WrapperTsIn>
+    TimedReturnedObject<void> TimeFunction(std::function<void(FuncTsIn...)> func, WrapperTsIn... args)
+    {
+        TimedReturnedObject<void> retObj;
+
+        auto start = chrono::high_resolution_clock::now();
+        func(std::forward<WrapperTsIn>(args)...);
+        auto end = chrono::high_resolution_clock::now();
+
+        retObj.Nanoseconds = end - start;
+        return retObj;
+    }
+
+    template<class CL, class... FuncTsIn, class... WrapperTsIn>
+    TimedReturnedObject<void> TimeFunction(CL* self, void(CL::*func)(FuncTsIn...), WrapperTsIn... args)
+    {
+        TimedReturnedObject<void> retObj;
+
+        auto start = chrono::high_resolution_clock::now();
+        (self->*func)(std::forward<WrapperTsIn>(args)...);
+        auto end = chrono::high_resolution_clock::now();
+
+        retObj.Nanoseconds = end - start;
+        return retObj;
+    }
+    
+    template<class CL, class... FuncTsIn, class... WrapperTsIn>
+    TimedReturnedObject<void> TimeFunction(const CL* self, void(CL::*func)(FuncTsIn...) const, WrapperTsIn... args)
+    {
+        TimedReturnedObject<void> retObj;
+
+        auto start = chrono::high_resolution_clock::now();
+        (self->*func)(std::forward<WrapperTsIn>(args)...);
+        auto end = chrono::high_resolution_clock::now();
+
+        retObj.Nanoseconds = end - start;
         return retObj;
     }
 
